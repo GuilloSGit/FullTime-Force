e958a2015a9c501f103e22a93c275ea76f3680ac
Guillermo Andrada <guillermoandrada@gmail.com>
2023-11-25 02:19:17 -0300

    feat: Commits counter alarm in UI.
    Developed solution to achieve an alarm whenever a new commits hits on GitHub

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -3,37 +3,43 @@ import { getCommitsRequest } from '../api/commits';
 
 function Nav() {
   const [newCommitsCount, setNewCommitsCount] = useState(0);
+  const [initialCount, setInitialCount] = useState(0);
+  const [newCommit, setNewCommit] = useState(false)
 
-  const fetchNewCommitsCount = async () => {
+  const fetchInitialCount = async () => {
     try {
       const response = await getCommitsRequest();
       const data = await response.json();
-      setNewCommitsCount(data.commits.length);
+      setInitialCount(data.commits.length);
     } catch (error) {
       console.error('Error fetching new commits count:', error);
     }
   };
 
   useEffect(() => {
-    fetchNewCommitsCount();
-    const intervalId = setInterval(fetchNewCommitsCount, 60000);
-
+    const intervalId = setInterval(fetchInitialCount, 20000);
     return () => clearInterval(intervalId);
-  }, []);
+  })
+
+  useEffect(() => {
+    if(newCommitsCount > initialCount) {
+      setNewCommit(true);
+    }
+  }, [newCommitsCount]);
 
   return (
-    <nav className={`bg-white border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full z-1 ${newCommitsCount > 0 ? 'bg-red-500' : ''}`}>
+    <nav className={`bg-white border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full z-1 ${newCommit ? 'bg-red-500' : ''}`}>
       <div className="max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="../src/assets/logo.png" className="h-8" alt="GitHub Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FullTime Force Challenge</span>
         </a>
         {newCommitsCount > 0 && (
-          <div className="flex items-center">
-            <div className="w-6 h-6 mr-1 pr-1 bg-yellow-500 text-xs rounded-full flex items-center justify-center">
+          <div className="flex items-center hover:cursor-pointer hover:text-slate-300">
+            <div className="w-6 h-6 mr-1 pr-1 bg-gray-500 hover:bg-gray-600 text-xs rounded-full flex items-center justify-center">
               {newCommitsCount}
             </div>
-            <span className="text-gray-700 text-xs m-1">Commits</span>
+            <span className="text-gray-700 text-xs m-1 hover:text-slate-300">Commits</span>
           </div>
         )}
       </div>
