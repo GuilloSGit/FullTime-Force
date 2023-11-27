8069e8c0a9a92f097b11bbbeaf090250d0a3fa74
Guillermo Andrada <guillermoandrada@gmail.com>
2023-11-27 12:08:44 -0300

    Fix: Logo source changed

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -4,7 +4,7 @@ function Nav() {
     <nav className="border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full z-1">
       <div className="max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse md:align-middle">
-          <img src='../../public/logo.png' className="h-8" alt="GitHub Logo" />
+          <img src='logo.png' className="h-8" alt="GitHub Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FullTime Force Challenge</span>
         </a>
       </div>
