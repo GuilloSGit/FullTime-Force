73b69722407282a0d621ddf85b00fa133d02d62b
Guillermo Andrada <guillermoandrada@gmail.com>
2023-11-27 11:24:26 -0300

    Deploy: Deployment configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommitsModule } from './commits/commits.module';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [CommitsModule],
+  imports: [CommitsModule,
+    ConfigModule.forRoot()],
 })
 
-export class AppModule {}
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api')
-  await app.listen(3000);
+  const port = app.get(ConfigService).get('PORT')
+  await app.listen(port || 3000);
 }
 
 bootstrap();
